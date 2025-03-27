08c2ec2dc0c90c1c85385dcd73d90c6211699e94
20101rkdtjgus <shkang081222@gmail.com>
2025-03-27 14:57:17 +0900

    학습파일

---
+++ p51.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+char dna;
+int main() {
+	scanf("%c",&dna);
+	if(dna=='A'|| dna=='a')
+	{
+		printf("ADENINE");
+	}
+	else if(dna=='T'|| dna=='t')
+	{
+		printf("THYMINE");
+	}
+	else if(dna=='G'|| dna=='g')
+	{
+		printf("GUANINE");
+	}
+	else if(dna=='C'|| dna=='c')
+	{
+		printf("CYTOSINE");
+	}
+	else printf("잘못입력했습니다");
+	
+	return 0;
+}
+
